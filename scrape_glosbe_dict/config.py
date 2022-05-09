134efcee2fd57e3323176d33e6cb641796ad8813
freemt <yucongo+fmt@gmail.com>
2022-05-09 13:50:58 +0800

    Update reatelimit, tenacy.retry back

--- scrape_glosbe_dict/config.py
+++ scrape_glosbe_dict/config.py
@@ -6,12 +6,15 @@ from pathlib import Path
 from logzero import logger
 from pydantic import BaseSettings, validator
 
+default_calls = 6
+default_period = 60
+
 
 class Settings(BaseSettings):  # pylint: disable=too-few-public-methods
     """Manage calls and period in secs for ratelimit."""
 
-    calls: float = 6
-    period: float = 60  # s
+    calls: float = default_calls
+    period: float = default_period  # s
 
     class Config:  # pylint: disable=too-few-public-methods
         """Read envs and env."""
@@ -29,10 +32,10 @@ class Settings(BaseSettings):  # pylint: disable=too-few-public-methods
         """Validate calls."""
         if not isinstance(v, float):  # wont come to this
             logger.warning("Float only, setting to default 150")
-            v = 100
+            v = default_calls
         if v <= 0:
             logger.warning("Makes no sense, setting to default 150")
-            v = 100
+            v = default_calls
 
         return v
 
@@ -41,10 +44,10 @@ class Settings(BaseSettings):  # pylint: disable=too-few-public-methods
         """Validate period."""
         if not isinstance(v, float):  # unnecessary
             logger.warning("Float only, setting to default 150")
-            v = 900
+            v = default_period
         if v <= 0:
             logger.warning("Makes no sense, setting to default 150")
-            v = 900
+            v = default_period
 
         if "calls" in values and values["calls"] / v * 3600 > 400:
             logger.warning(
--- tests/test_config.py
+++ tests/test_config.py
@@ -7,20 +7,20 @@ from scrape_glosbe_dict.config import Settings
 def test_config_normal():
     """Test config normal."""
     config = Settings()
-    assert config.calls == 100
-    assert config.period == 900
+    assert config.calls == 6
+    assert config.period == 60
 
 
 @pytest.mark.xfail
 def test_config_calls_period_str():
     """Test config normal."""
     config = Settings(calls="a", period="b")
-    assert config.calls == 100
-    assert config.period == 900
+    assert config.calls == 6
+    assert config.period == 60
 
 
 def test_config_calls_period_minus():
     """Test config normal."""
     config = Settings(calls=-1, period=-2)
-    assert config.calls == 100
-    assert config.period == 900
+    assert config.calls == 6
+    assert config.period == 60
